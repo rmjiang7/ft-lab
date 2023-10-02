040b4cdea096eeacb6ddcfa669ceecab2011ce8a
rmjiang7 <rmjiang7@gmail.com>
2023-10-01 18:05:57 -0700

    Add working scripts

---
+++ llm/inference/inference.py
@@ -0,0 +1,58 @@
+from datasets import load_dataset
+from random import randrange
+import torch
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
+
+from transformers import TrainingArguments
+
+args = TrainingArguments(
+    output_dir="llama-7-int4-dolly",
+    num_train_epochs=3,
+    per_device_train_batch_size=6,
+    gradient_accumulation_steps=2,
+    gradient_checkpointing=True,
+    optim="paged_adamw_32bit",
+    logging_steps=10,
+    save_strategy="epoch",
+    learning_rate=2e-4,
+    bf16=True,
+    tf32=True,
+    max_grad_norm=0.3,
+    warmup_ratio=0.03,
+    lr_scheduler_type="constant",
+    disable_tqdm=True # disable tqdm since with packing values are in correct
+)
+
+args.output_dir = "llama-7-int4-dolly"
+
+# load base LLM model and tokenizer
+model = AutoPeftModelForCausalLM.from_pretrained(
+    args.output_dir,
+    low_cpu_mem_usage=True,
+    torch_dtype=torch.float16,
+    load_in_4bit=True,
+)
+tokenizer = AutoTokenizer.from_pretrained(args.output_dir)
+
+
+# Load dataset from the hub and get a sample
+dataset = load_dataset("databricks/databricks-dolly-15k", split="train")
+sample = dataset[randrange(len(dataset))]
+
+prompt = f"""### Instruction:
+Use the Input below to create an instruction, which could have been used to generate the input using an LLM.
+
+### Input:
+{sample['response']}
+
+### Response:
+"""
+
+input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.cuda()
+# with torch.inference_mode():
+outputs = model.generate(input_ids=input_ids, max_new_tokens=100, do_sample=True, top_p=0.9,temperature=0.9)
+
+print(f"Prompt:\n{sample['response']}\n")
+print(f"Generated instruction:\n{tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0][len(prompt):]}")
+print(f"Ground truth:\n{sample['instruction']}")
